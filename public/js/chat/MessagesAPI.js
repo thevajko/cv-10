f3dd33572a529e80d261435c2f95816f7233f22f
hrkut <Patrik.Hrkut@fri.uniza.sk>
2024-12-03 08:16:23 +0100

    sendMessage

--- public/js/chat/MessagesAPI.js
+++ public/js/chat/MessagesAPI.js
@@ -15,7 +15,13 @@ class MessagesAPI extends DataService {
      * @returns {Promise<Array<Object>>}
      */
     async getMessages(lastID = null) {
-        // TODO Implement this method
+        let lastIdString = lastID == null ? "" : "&lastId=" + lastID;
+        return await this.sendRequest(
+            "getMessages" + lastIdString,
+            "POST",
+            200,
+            null,
+            []);
     }
 
     /**
@@ -25,7 +31,15 @@ class MessagesAPI extends DataService {
      * @returns {Promise<boolean>} true if message was sent
      */
     async sendMessage(message, recipient = null) {
-        // TODO Implement this method
+        return await this.sendRequest(
+            "saveMessage",
+            "POST",
+            204,
+            {
+                'recipient': recipient,
+                'message': message
+            },
+            false);
     }
 }
 
