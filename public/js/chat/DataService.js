b77674bb7ce2bbc6e9b6193bb4299da086b244b8
Patrik Hrkut <Patrik.Hrkut@fri.uniza.sk>
2025-12-01 18:01:34 +0100

    Implement error handling and response validation in sendRequest method

--- public/js/chat/DataService.js
+++ public/js/chat/DataService.js
@@ -37,7 +37,32 @@ class DataService {
      * @returns {Promise<any|any>} Return Promise, because this method uses fetch method
      */
     async sendRequest(action, method, responseCode, body, onErrorReturn = null) {
-        // TODO Implement this method
+        // Use exceptions to wrap the fetch call
+        try {
+            // Build up fetch and wait for response
+            let response = await fetch(
+                this.#url(action), // URL to the action
+                {
+                    method: method,
+                    body: JSON.stringify(body),
+                    headers: { // Set headers for JSON communication
+                        'X-Requested-With': 'XMLHttpRequest', // Indicate AJAX request
+                        "Content-type": "application/json", // Send JSON
+                        "Accept": "application/json", // Accept only JSON as response
+                    }
+                });
+            // If return code do not match our expected value throw error
+            if (response.status !== responseCode) return onErrorReturn;
+
+            // If the response is empty (HTTP 204 No content), it's ok
+            if (response.status === 204) return true;
+
+            // Everything is ok, send the response
+            return await response.json();
+        } catch (ex) {
+            // On any error just return error
+            return onErrorReturn;
+        }
     }
 }
 
